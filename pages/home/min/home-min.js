73ec4d03c0254f18e48d4d822096a6dda12195af
e.balashov <e.balashov@rambler-co.ru>
2017-06-29 00:05:54 +0300

    init home page

---
+++ pages/home/min/home-min.js
@@ -0,0 +1,18 @@
+const MOBILE_DEVICES = /Android|androidAfishaRestApp|webOS|iPhone|iPod|iosAfishaRestApp|BlackBerry|IEMobile|windowsAfishaRestApp|Opera Mini/i
+
+const USER_AGENT = (MOBILE_DEVICES.test(window.navigator.userAgent)) ? 'mobile' : 'desktop'
+
+if (USER_AGENT === 'mobile') {
+  window.document.body.classList.add('mobile');
+} else {
+  window.document.body.classList.add('desktop');
+}
+
+if (document.documentElement.clientHeight
+  < document.documentElement.clientWidth) {
+  window.document.body.classList.add('wide');
+} else {
+  window.document.body.classList.add('narrow');
+}
+
+
